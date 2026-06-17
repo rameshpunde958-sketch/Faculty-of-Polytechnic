958186723cbc1e9991c807becefabe8b4e9e2c7f
rameshpunde958-sketch <rameshpunde958@gmail.com>
2026-06-17 09:52:35 +0530

    Add function to calculate average of three numbersI
    Implement a function to calculate the average of three numbers and print the results for three sets of inputs.

---
+++ students/bhakti-punde/Solution 13.py
@@ -0,0 +1,7 @@
+#problem 1
+def average_of_three(m1, m2, m3):
+    avg = (m1 + m2 + m3) / 3
+    return round(avg,1)
+print(average_of_three(85, 90, 78))
+print(average_of_three(30, 28, 35))
+print(average_of_three(40, 55, 70))  
